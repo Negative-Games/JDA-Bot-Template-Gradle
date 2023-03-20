bf6912396ab99465e3c5837a192421cee439b87e
Negative <negggative@gmail.com>
2023-03-20 14:44:24 -0300

    Update Bot.java

--- src/main/java/games/negative/bot/Bot.java
+++ src/main/java/games/negative/bot/Bot.java
@@ -1,6 +1,8 @@
 package games.negative.bot;
 
 import games.negative.framework.discord.DiscordBot;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import java.util.List;
 
@@ -8,9 +10,15 @@ public class Bot extends DiscordBot {
 
     public Bot() {
 
-        String token = "TOKEN";
-        create(token, List.of())
 
+        String token = "TOKEN";
+
+        List<GatewayIntent> intents = List.of(
+                GatewayIntent.MESSAGE_CONTENT,
+                GatewayIntent.GUILD_MEMBERS
+        );
+
+        JDABuilder builder = create(token, intents);
 
     }
 }
