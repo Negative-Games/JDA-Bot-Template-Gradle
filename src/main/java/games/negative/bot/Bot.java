b4ad17ca3f603d02d1aacac21ca20e18431ce81c
Negative <negggative@gmail.com>
2023-03-20 15:07:05 -0300

    Update Bot.java

--- src/main/java/games/negative/bot/Bot.java
+++ src/main/java/games/negative/bot/Bot.java
@@ -1,6 +1,7 @@
 package games.negative.bot;
 
 import games.negative.framework.discord.DiscordBot;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
@@ -17,5 +18,17 @@ public class Bot extends DiscordBot {
 
         JDABuilder builder = create(token, intents);
 
+        // In this middle part is where you should be
+        // registering most of your own logic, commands, etc.
+
+        JDA jda;
+        try {
+            jda = builder.build().awaitReady();
+        } catch (InterruptedException e) {
+            System.out.println("Failed to build JDA");
+            throw new RuntimeException(e);
+        }
+
+        initializeCommands(jda);
     }
 }
